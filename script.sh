02b5f2f2cdedbd45e3a12b3bebc131e72ae1f7ab
Sameerana Hammigi <sameeranahammigi@gmail.com>
2024-09-25 16:11:41 +0530

    first

---
+++ script.sh
@@ -0,0 +1,20 @@
+sudo apt-get update
+
+
+sudo apt-get install -y git
+
+
+sudo apt-get install -y openjdk-11-jdk
+
+
+sudo apt-get install -y maven
+
+
+wget -q -O - https://pkg.jenkins.io/debian/jenkins.io.key
+
+sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+
+sudo apt-get update
+
+sudo apt-get install -y jenkins
